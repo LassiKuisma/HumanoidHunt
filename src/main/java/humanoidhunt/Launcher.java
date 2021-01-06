a99513d4493268651db86cdb6c6173885bca1acc
Lassi Kuisma <lassi.kuisma1@gmail.com>
2021-01-06 21:56:42 +0200

    setting up things

---
+++ src/main/java/humanoidhunt/Launcher.java
@@ -0,0 +1,9 @@
+package humanoidhunt;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        System.out.println("HIIIYYYEEEE!");
+    }
+
+}
---
+++ src/test/java/humanoidhunt/BasicTest.java
@@ -0,0 +1,14 @@
+package humanoidhunt;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BasicTest {
+
+    @Test
+    public void testsRunSuccessfully() {
+        assertEquals(4, 2 + 2);
+    }
+
+}
